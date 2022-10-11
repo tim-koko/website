4f9ae1df7d7582a0ef41697bb5672b8115969e3f
Fint <mail@fint.ch>
2022-10-11 15:04:57 +0200

    more icons

--- assets/js/slideshow.js
+++ assets/js/slideshow.js
@@ -1,8 +1,8 @@
 import  Flickity from 'flickity';
 
   
-  console.log('slideshow', slideshow)
-
+  //console.log('slideshow', slideshow)
+/*
   const slideshow = document.getElementById('slideshow');
 
     if(slideshow) {
@@ -25,4 +25,82 @@ import  Flickity from 'flickity';
             }, 
         });
     }
+*/
 
+// Play with this value to change the speed
+let tickerSpeed = 2;
+
+let flickity = null;
+let isPaused = false;
+const slideshowEl = document.getElementById('slideshow');
+
+
+//
+//   Functions
+//
+//////////////////////////////////////////////////////////////////////
+
+const update = () => {
+  if (isPaused) return;
+  if (flickity.slides) {
+    flickity.x = (flickity.x - tickerSpeed) % flickity.slideableWidth;
+    flickity.selectedIndex = flickity.dragEndRestingSelect();
+    flickity.updateSelectedSlide();
+    flickity.settle(flickity.x);
+  }
+  window.requestAnimationFrame(update);
+};
+
+const pause = () => {
+  isPaused = true;
+  tickerSpeed = 0.5;
+};
+
+const play = () => {
+    tickerSpeed = 2;
+
+  if (isPaused) {
+    isPaused = false;
+    window.requestAnimationFrame(update);
+    }
+};
+
+
+//
+//   Create Flickity
+//
+//////////////////////////////////////////////////////////////////////
+
+flickity = new Flickity(slideshowEl, {
+  autoPlay: false,
+  prevNextButtons: false,
+  pageDots: false,
+  draggable: true,
+  wrapAround: true,
+  selectedAttraction: 0.015,
+  friction: 0.25
+});
+flickity.x = 0;
+
+
+//
+//   Add Event Listeners
+//
+//////////////////////////////////////////////////////////////////////
+
+slideshowEl.addEventListener('mouseenter', pause, false);
+slideshowEl.addEventListener('focusin', pause, false);
+slideshowEl.addEventListener('mouseleave', play, false);
+slideshowEl.addEventListener('focusout', play, false);
+
+flickity.on('dragStart', () => {
+  isPaused = true;
+});
+
+
+//
+//   Start Ticker
+//
+//////////////////////////////////////////////////////////////////////
+
+update();
