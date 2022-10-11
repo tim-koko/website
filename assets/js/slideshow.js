28b8189793d8d45d8c1174e8ff2e917cef4c9851
Fint <mail@fint.ch>
2022-10-11 21:18:09 +0200

    slideshow test

--- assets/js/slideshow.js
+++ assets/js/slideshow.js
@@ -1,5 +1,5 @@
 import  Flickity from 'flickity';
-
+import anime from 'animejs';
   
   //console.log('slideshow', slideshow)
 /*
@@ -28,10 +28,10 @@ import  Flickity from 'flickity';
 */
 
 // Play with this value to change the speed
-let tickerSpeed = 2;
+//let tickerSpeed = 2;
 
 let flickity = null;
-let isPaused = false;
+//let isPaused = false;
 const slideshowEl = document.getElementById('slideshow');
 
 
@@ -39,7 +39,7 @@ const slideshowEl = document.getElementById('slideshow');
 //   Functions
 //
 //////////////////////////////////////////////////////////////////////
-
+/*
 const update = () => {
   if (isPaused) return;
   if (flickity.slides) {
@@ -64,7 +64,7 @@ const play = () => {
     window.requestAnimationFrame(update);
     }
 };
-
+*/
 
 //
 //   Create Flickity
@@ -76,9 +76,10 @@ flickity = new Flickity(slideshowEl, {
   prevNextButtons: false,
   pageDots: false,
   draggable: true,
-  wrapAround: true,
-  selectedAttraction: 0.015,
-  friction: 0.25,
+  //wrapAround: true,
+  freeScroll: true,
+  //selectedAttraction: 0.015,
+  //friction: 0.25,
 });
 flickity.x = 0;
 
@@ -87,7 +88,7 @@ flickity.x = 0;
 //   Add Event Listeners
 //
 //////////////////////////////////////////////////////////////////////
-
+/*
 slideshowEl.addEventListener('mouseenter', pause, false);
 slideshowEl.addEventListener('focusin', pause, false);
 slideshowEl.addEventListener('mouseleave', play, false);
@@ -104,3 +105,30 @@ flickity.on('dragStart', () => {
 //////////////////////////////////////////////////////////////////////
 
 update();
+*/
+
+function getScrollPercent() {
+    var h = document.documentElement, 
+        b = document.body,
+        st = 'scrollTop',
+        sh = 'scrollHeight';
+    return (h[st]||b[st]) / ((h[sh]||b[sh]) - h.clientHeight) * 100;
+}
+
+const divAnimation = anime({
+    targets: '.flickity-slider',
+    translateX: [100, 0],
+    //elasticity: 200,
+    easing: 'easeInOutCubic',
+    autoplay: false,
+    duration: 200,
+  });
+
+  /**
+ * Add a scroll listener on the window object to
+ * control animations based on scroll percentage.
+ */
+window.onscroll = () => {
+    divAnimation.seek(( (getScrollPercent()) / 70) * divAnimation.duration);
+  };
+  
