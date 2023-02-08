099bcc2a13d81b6a12fae8fd2a5fa43b79016208
Fint <mail@fint.ch>
2023-02-08 09:32:30 +0100

    scroll to anchor fix

--- assets/js/scroll-top.js
+++ assets/js/scroll-top.js
@@ -23,3 +23,44 @@ window.addEventListener('scroll', () => {
   getScrollPercent() > 70 ? btnTop.classList.add('show') : btnTop.classList.remove('show');
   getScrollPercent() > 98 ? btnTop.classList.add('bottom') : btnTop.classList.remove('bottom');
 }, {passive: true});
+
+
+
+function offset(el) {
+  const box = el.getBoundingClientRect(),
+      docElem = document.documentElement;
+
+  return {
+    top: box.top + window.pageYOffset - docElem.clientTop,
+    left: box.left + window.pageXOffset - docElem.clientLeft,
+  };
+}
+
+
+function scrollTo (target) {
+
+  const scrollElement = window.document.scrollingElement || window.document.body || window.document.documentElement,
+         targetEl = document.querySelector(target),
+         topEl = document.querySelector('body'),
+         destination =  offset(targetEl).top  - offset(topEl).top;
+    
+    anime({
+      targets: scrollElement,
+      scrollTop: (destination > window.scrollY ? destination-50 : destination-50),
+      duration: 666,
+      easing: 'easeInOutExpo',
+    });
+    
+}
+
+
+const menuLinks = document.querySelectorAll('.home .menu-link');
+
+menuLinks.forEach(el => {
+  el.addEventListener('click', (e) => {
+    e.preventDefault();
+    
+    //console.log(e.target.hash)
+    scrollTo (e.target.hash);
+  });
+});
