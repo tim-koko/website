36a52470089740e29298de80a1d6d6e654bd2d93
jan <mail@fint.ch>
2024-05-28 19:52:10 +0200

    set loop to false with js

--- assets/js/logo-animation.js
+++ assets/js/logo-animation.js
@@ -1,32 +1,25 @@
 import '@dotlottie/player-component';
+//import '@lottiefiles/lottie-player'
 import { create } from '@lottiefiles/lottie-interactivity';
  
-
-
 const logo = document.querySelector('#logoLottie');
+
 if(logo) {
-  /*
-  logo.addEventListener('ready', () => {
-    logo.setLooping(false);
-    logo.getLottie().setLooping(false);
 
-  });
-*/
+    logo.setAttribute('loop', 'false');
 
-  logo.addEventListener('complete', () => {
 
-    create({
-      player: logo.getLottie(),
-      mode: 'cursor',
-      actions: [
-        {
-          type: 'hover',
-          forceFlag: false,
-        },
-      ],
+    logo.addEventListener('complete', () => {
+      create({
+        player: logo.getLottie(),
+        mode: 'cursor',
+        actions: [
+          {
+            type: 'hover',
+            forceFlag: false,
+          },
+        ],
+      });
     });
-  //}, 100);
-  });
- 
 
 }
