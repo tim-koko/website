c61b0924e08a15c413e8cd2fd9be50cc649762d1
jan <mail@fint.ch>
2024-06-03 13:35:37 +0200

    new logo animation (canvas)

--- assets/js/logo-animation.js
+++ assets/js/logo-animation.js
@@ -1,56 +1,42 @@
-//import '@dotlottie/player-component';
-//import '@lottiefiles/lottie-player'
-//import { DotLottie } from '@lottiefiles/dotlottie-web';
 import { DotLottie } from '@lottiefiles/dotlottie-web';
-//import { DotLottie } from 'https://cdn.jsdelivr.net/npm/@lottiefiles/dotlottie-web/+esm';
-
-import { create } from '@lottiefiles/lottie-interactivity';
- 
-setTimeout(() => {
 
 const logoContainer = document.querySelector('.logo');
 const logocanvas = document.querySelector('#logolottie');
 
 const src = '/timkoko.lottie';
-//const src = "https://lottie.host/9ac9c440-c19e-4ac9-b60e-869b6d0ef8cb/7h97gYMCNE.lottie";
-
-
-console.log(logocanvas);
 
 if(logocanvas) {
   logoContainer.classList.add('show');
 
   const logoAnimation = new DotLottie({
-    logocanvas,
+    canvas: logocanvas,
     src,
     loop: false,
     autoplay: true,
+     layout: {
+      fit : 'cover',
+      align: [0.5,0.5],
+    },
   });
 
-
-
+  // show fallback svg:
   logoAnimation.addEventListener('loadError', () => {
     document.querySelector('.logo-alt').classList.remove('d-none');
+    logocanvas.classList.add('d-none');
   });
-
+  
   logoAnimation.addEventListener('error', () => {
     document.querySelector('.logo-alt').classList.remove('d-none');
+    logocanvas.classList.add('d-none');
   });
 
-  logoAnimation.addEventListener('complete', () => {
-    create({
-      player: logoAnimation.getLottie(),
-      mode: 'cursor',
-      actions: [
-        {
-          type: 'hover',
-          forceFlag: false,
-        },
-      ],
-    });
+  // play again on mouseover:
+  logocanvas.addEventListener('mouseover', () => {
+    logoAnimation.play();
   });
-
-}
  
 
-}, 500);
+  console.log(logoAnimation)
+
+ 
+}
