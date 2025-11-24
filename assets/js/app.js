c2dcd91f038cdbeef12f20669bdba42951ee845d
jan <mail@fint.ch>
2025-11-24 10:23:22 +0100

    replaced flickity with swiper.js code cleanup font sizes etc

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,7 +7,6 @@
 import Swup from 'swup';
 import SwupBodyClassPlugin from '@swup/body-class-plugin';
 import SwupHeadPlugin from '@swup/head-plugin';
-//import SwupScrollPlugin from '@swup/scroll-plugin';
 //import SwupDebugPlugin from '@swup/debug-plugin';
 
 import starterScripts from './assets/js/views/starter-scripts';
@@ -34,18 +33,9 @@ import {
 const swup = new Swup({
   containers: ['#main', '#main-menu' , '.language-menu' ],
   ignoreVisit: (url, { el } = {}) => el.closest('.language-menu') ,
-  //ignoreVisit: (url, { el } = {}) => el?.closest('[data-no-swup]') || el?.closest('#wpadminbar'),
-  //native: true,
   plugins: [
     new SwupBodyClassPlugin(),
     new SwupHeadPlugin({ persistAssets: true }),
-   /*
-    new SwupScrollPlugin({
-      //doScrollingRightAway: true,
-      //animateScroll: false
-    }),
-    */
-    //new SwupDebugPlugin(),
   ],
 });
 
@@ -58,38 +48,3 @@ swup.hooks.on('page:view', (visit) => {
   starterScripts();
   onEveryPage();
 
-
-
-// detect Windows and touch devices
-//import './assets/js/check-os-touch';
-
-// lazy sizes for image loading
-//import 'lazysizes';
-
-// global alert
-//import './assets/js/alert';
-
-// home 3D animation
-//import './assets/js/home-animation';
-
-// fade in elements
-//import './assets/js/scroll-out';
-
-// icons animations
-//import './assets/js/icon-animations';
-
-// slideshow
-//import './assets/js/slideshow';
-
-// swiper sliders
-//import './assets/js/swiper';
-
- 
-
-//import './assets/js/scroll-top';
-
-// logo animation
-//import './assets/js/logo-animation';
-
-// form netlify submission 
-//import './assets/js/form-netlify';
--- assets/js/views/starter-scripts.js
+++ assets/js/views/starter-scripts.js
@@ -6,25 +6,21 @@ import checkOSTouch from 'js/utils/check-os-touch';
 export default function() {
 
   checkOSTouch();
+  scrollTop();
+  logoAnimation();
 
     /* Headroom */
    var myElement = document.querySelector('.banner');
     var headroom  = new Headroom(myElement, {tolerance : 5, offset : 10});
     headroom.init();
    
-  
 
     /* Mobile Menu */
    const myOffCanvas = document.getElementById('offcanvasMenu');
-const body = document.querySelector('body');
+    const body = document.querySelector('body');
 
-myOffCanvas.addEventListener('show.bs.offcanvas', () => body.classList.add('menu-open') );
-myOffCanvas.addEventListener('hide.bs.offcanvas', () => body.classList.remove('menu-open') );
+    myOffCanvas.addEventListener('show.bs.offcanvas', () => body.classList.add('menu-open') );
+    myOffCanvas.addEventListener('hide.bs.offcanvas', () => body.classList.remove('menu-open') );
 
- scrollTop();
-  logoAnimation();
-
- 
- 
 
 }
